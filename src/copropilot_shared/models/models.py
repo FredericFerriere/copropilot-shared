8b1474e373fc9e3a594e407133bf5b77aacea912
Frederic Ferriere <57955693+FredericFerriere@users.noreply.github.com>
2026-06-01 18:23:20 +0200

    pydantic classes

--- src/copropilot_shared/models/models.py
+++ src/copropilot_shared/models/models.py
@@ -1,6 +1,18 @@
+import enum
+from typing import List
+
 from pydantic import BaseModel
 from datetime import date
 from decimal import Decimal
+
+class typeAssemblee(str, enum.Enum):
+    ORDINAIRE = "ordinaire"
+    EXTRAORDINAIRE = "extraordinaire"
+    MIXTE = "mixte"
+
+class typeContenu(str, enum.Enum):
+    ORDRE_DU_JOUR = "ordre_du_jour"
+    ETAT_FINANCIER = "etat_financier"
 
 class Copropriete(BaseModel):
     id: int | None = None
@@ -17,10 +29,49 @@ class Exercice(BaseModel):
 
     model_config = {"from_attributes": True}
 
+class LigneEtatFinancier(BaseModel):
+    id: int | None = None
+    etat_financier_id: int
+    numero_compte: str
+    montant_cents: int | str
+
+    model_config = {"from_attributes": True}
+
 class EtatFinancier(BaseModel):
     id: int | None = None
     exercice_id: int
-    numero_compte: int
-    montant_cents: int
+    lignes_etat_financier: List[LigneEtatFinancier] = []
+    model_config = {"from_attributes": True}
+
+class AssembleeGenerale(BaseModel):
+    id: int | None = None
+    date: date
+    copropriete_id: int
+    type: typeAssemblee
+
+    model_config = {"from_attributes": True}
+
+class Resolution(BaseModel):
+    id: int | None = None
+    assemblee_id: int
+    numero: str
+    libelle: str
+    regle_majorite: str
+    detail_resolution: str
+
+    model_config = {"from_attributes": True}
+
+class OrdreDuJour(BaseModel):
+    id: int | None = None
+    assemblee_id: int
+    resolutions: List[Resolution] = []
+
+    model_config = {"from_attributes": True}
+
+class ContenuDocument(BaseModel):
+    id: int | None = None
+    document_id: int
+    type_contenu: typeContenu
+    contenu_id: int
 
     model_config = {"from_attributes": True}
